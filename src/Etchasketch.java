1c75df719a07396a5e63a14c8562709a40176824
Andrei Frankovic <frankovica@rsd407.org>
2026-09-14 10:00:17 -0700

    Started on Etchsketch

---
+++ src/Etchasketch.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+public class Etchasketch extends JFrame
+{
+    public Etchasketch() 
+    {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Player Movement");
+        setSize(400, 400);
+        setResizable(false);
+
+        init();
+    }
+    public void init()
+    {
+        setLayout(new )
+    }
+    public static void main(String[] args)
+    {
+        Etchasketch f = new Etchasketch();
+    }
+}
