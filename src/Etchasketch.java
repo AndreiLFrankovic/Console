937b14da02033eb4c8129c47f39985b0de0a2263
Andrei Frankovic <frankovica@rsd407.org>
2026-09-15 08:01:29 -0700

    Worked more on player movement

--- src/Etchasketch.java
+++ src/Etchasketch.java
@@ -1,21 +1,29 @@
-import javax.swing.JFrame;
-public class Etchasketch extends JFrame
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+public class Etchasketch extends JPanel 
 {
-    public Etchasketch() 
+        
+        private int playerX = 100;
+        private int playerY = 100;
+        private final int Player_Speed = 40;
+        private final int Move_Speed = 5;
+    public Etchasketch()
     {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Player Movement");
-        setSize(400, 400);
-        setResizable(false);
+        setPreferredSize(new Dimension(500, 500));
+        setBackground(Color.BLACK);
+        setupKeyBindings();
 
-        init();
     }
-    public void init()
+    private void setupKeyBindings()
     {
-        setLayout(new )
+    InputMap inputMap = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+    ActionMap actionMap = getActionMap();
+    inputMap.put(KeyStroke.getKeyStroke("W"), "Move Up");
+    inputMap.put(KeyStroke.getKeyStroke( "S"), "Move Down");
+    inputMap.put(KeyStroke.getKeyStroke("A"),"Move Left");
+    inputMap.put(KeyStroke.getKeyStroke("D"), "Move Right");
     }
-    public static void main(String[] args)
-    {
-        Etchasketch f = new Etchasketch();
-    }
-}
+
+    action 
+}   
